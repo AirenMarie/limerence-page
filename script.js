1deb2775a6dfc0e173e3cb60191da1a3156208b7
AirenMarie <astringer92@gmail.com>
2025-11-16 23:52:21 -0500

    adjusted HTML some more, declared variables for DOM elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle the survey and info
+// Toggle navigation links
+const navbar = document.getElementById("navbar");
+const surveySection = document.getElementById("brief-survey");
+const submitBtn = document.getElementById("submit-btn");
