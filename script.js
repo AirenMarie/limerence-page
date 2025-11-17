00b2c39ba4682c113fa1eb67394e8e3e7c97d0c9
AirenMarie <astringer92@gmail.com>
2025-11-17 17:48:56 -0500

    Everything just flashes. I give up.

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 // Toggle the survey and info
 // Toggle navigation links
 const navbar = document.getElementById("navbar");
-let surveySection = document.getElementById("brief-survey");
+let surveyPortions = document.getElementsByClassName("survey-portion");
 const submitBtn = document.getElementById("submit-btn");
 let pageSections = document.getElementsByClassName("page-section");
+let thanksMsg = document.getElementById("thank-you-msg");
 
 submitBtn.addEventListener("click", () => {
-  // surveySection.classList.toggle("is-hidden");
-  surveySection.innerHTML = `<p id="thank-you-msg">Thanks for your feedback!</p>`;
+  Array.from(surveyPortions).forEach((portion) => {
+    portion.classList.toggle("is-hidden");
+  });
 
   Array.from(pageSections).forEach((section) => {
     section.classList.toggle("is-visible");
   });
+
+  thanksMsg.classList.toggle("is-visible");
 });
