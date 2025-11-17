1596d9ce544bf00dd8bb9a5f64aacc5817dc1c6f
AirenMarie <astringer92@gmail.com>
2025-11-17 01:31:14 -0500

    I just wanna toggle dammit

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // Toggle the survey and info
 // Toggle navigation links
 const navbar = document.getElementById("navbar");
-const surveySection = document.getElementById("brief-survey");
+let surveySection = document.getElementById("brief-survey");
 const submitBtn = document.getElementById("submit-btn");
+let pageSections = document.getElementsByClassName("page-section");
+
+submitBtn.addEventListener("click", () => {
+  surveySection.classList.toggle("is-hidden");
+
+  Array.from(pageSections).forEach((section) => {
+    section.classList.toggle("is-hidden");
+  });
+});
