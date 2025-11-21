fe373153e9fe9a96c907f4b08d9df0cf80bab591
AirenMarie <astringer92@gmail.com>
2025-11-20 19:40:34 -0500

    toggle is working

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 // Toggle the survey and info
 // Toggle navigation links
-/* let surveyPortions = document.querySelector(".survey-portion");
+let survey = document.getElementById("brief-survey");
+let infoSections = document.getElementById("info");
 const submitBtn = document.getElementById("submit-btn");
-let pageSections = document.querySelector(".page-section");
 let thanksMsg = document.getElementById("thank-you-msg");
 
 submitBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  Array.from(surveyPortions).forEach((portion) => {
-    portion.classList.toggle("is-hidden");
-  });
+  survey.classList.toggle("is-hidden");
 
-  Array.from(pageSections).forEach((section) => {
-    section.classList.toggle("is-visible");
-  });
+  infoSections.classList.toggle("is-visible");
 
   thanksMsg.classList.toggle("is-visible");
   thanksMsg.textContent = "Thank you for sharing!";
-}); */
+
+  submitBtn.disabled = true;
+});
